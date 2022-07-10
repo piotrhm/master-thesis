8099d297a45b7f4cee3164c5bef1aabb0281c8c2
piotrhm <helm.piotr@gmail.com>
2022-07-11 00:50:25 +0200

    simple mixup on cifar10c

--- src/datamodules/cifar10_datamodule.py
+++ src/datamodules/cifar10_datamodule.py
@@ -38,14 +38,14 @@ class CIFAR10DataModule(DataModule, ABC):
         self.transform_test = transforms.Compose([
             transforms.ToTensor(),
             transforms.Normalize((0.4914, 0.4822, 0.4465),
-                                  (0.2470, 0.2435, 0.2616)),
+                                 (0.2470, 0.2435, 0.2616)),
         ])
         self.transform_train = transforms.Compose([
             # transforms.RandomCrop(32, padding=4),
             # transforms.RandomHorizontalFlip(),
             transforms.ToTensor(),
             transforms.Normalize((0.4914, 0.4822, 0.4465),
-                                  (0.2470, 0.2435, 0.2616)),
+                                 (0.2470, 0.2435, 0.2616)),
         ])
 
     @property
