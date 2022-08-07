8e4bc42131d57f2a51ba351404af637459b367e3
piotrhm <helm.piotr@gmail.com>
2022-08-07 16:20:17 +0200

    bert-nlp mixup

--- src/models/manifold_mixup.py
+++ src/models/manifold_mixup.py
@@ -1,54 +1,70 @@
-from typing import Any
-
+import hydra
+import transformers
 import torch
 import composer.functional as cf
 
-from src.models.base_module import BaseModule
+from typing import Any
+from lightning_transformers.task.nlp.text_classification import TextClassificationTransformer
+from torch.nn import CrossEntropyLoss
 
 
-class ManifoldMixUpModule(BaseModule):
-    """Extension of BaseModule. Mixup from mosaicml support added.
-
-    Read the docs:
-        https://github.com/mosaicml/composer/tree/dev/composer/algorithms/mixup
-    """
-
+class TextClassificationTransformerWrapperMixup(TextClassificationTransformer):
     def __init__(
-        self,
-        net: Any,
-        optimizer: Any,
-        lr_scheduler: Any,
-        monitor: Any,
-        interpolate_loss=True,
-        alpha=0.2,
-    ):
+            self,
+            optimizer: Any,
+            lr_scheduler: Any,
+            monitor: Any,
+            pretrained_model_name_or_path,
+            num_labels: int,
+            alpha=1,
+    ) -> None:
         super().__init__(
-            net=net,
-            optimizer=optimizer,
-            lr_scheduler=lr_scheduler,
-            monitor=monitor
+            pretrained_model_name_or_path=pretrained_model_name_or_path,
+            num_labels=num_labels
         )
         self.save_hyperparameters(logger=False)
+        self.metrics = {}
+        self.criterion = CrossEntropyLoss()
 
-    def step_mixup(self, batch: Any):
-        x, y = batch
-        x, y_perm, mixing = cf.mixup_batch(x, y, alpha=self.hparams.alpha)
 
-        if self.hparams.interpolate_loss:
-            y_hat = self.forward(x)
-            loss = (1 - mixing) * self.criterion(y_hat, y) + mixing * self.criterion(y_hat, y_perm)
+    def training_step(self, batch: Any, batch_idx: int, dataloader_idx: int = 0) -> torch.Tensor:
+        if self.pretrained_model_name_or_path == "distilbert-base-uncased":
+            batch['inputs_embeds'] = self.model.distilbert.embeddings(batch['input_ids'])
+            batch.pop('input_ids', None)
         else:
-            y_mixed = (1 - mixing) * y + mixing * y_perm
-            y_hat = self.forward(x)
-            loss = self.criterion(y_hat, y_mixed)
+            raise NotImplementedError("training step is not implemented for model")
 
-        preds = torch.argmax(y_hat, dim=1)
-        return loss, preds, y
+        x, y_perm, mixing = cf.mixup_batch(batch['inputs_embeds'], batch['labels'], alpha=self.hparams.alpha)
+        batch.pop('inputs_embeds', None)
+        batch['inputs_embeds'] = x
 
-    def training_step(self, batch: Any, batch_idx: int):
-        loss, preds, targets = self.step_mixup(batch)
-        acc = self.train_acc(preds, targets)
-        self.log("train/loss", loss, on_step=False, on_epoch=True, prog_bar=False)
-        self.log("train/acc", acc, on_step=False, on_epoch=True, prog_bar=True)
+        assert self.model.config.problem_type == self.model.config.problem_type
 
-        return {"loss": loss, "preds": preds, "targets": targets}
+        outputs = self.model(**batch)
+        y_hat = outputs[1]
+        loss = (1 - mixing) * self.criterion(y_hat.view(-1, self.model.num_labels), batch['labels'].view(-1)) +\
+               mixing * self.criterion(y_hat.view(-1, self.model.num_labels), y_perm.view(-1))
+
+        self.log("train_loss", loss)
+        return loss
+
+    def configure_optimizers(self):
+        """
+        Choose what optimizers and learning-rate schedulers to use in your optimization.
+        Normally you'd need one. But in the case of GANs or similar you might have multiple.
+
+        See examples here:
+            https://pytorch-lightning.readthedocs.io/en/latest/common/lightning_module.html#configure-optimizers
+        """
+        config_opt = self.hparams.optimizer
+        config_opt.pop('_partial_')
+        opt = hydra.utils.instantiate(config_opt, params=self.parameters())
+
+        if "lr_scheduler" not in self.hparams:
+            return [opt]
+        scheduler = hydra.utils.instantiate(self.hparams.lr_scheduler, optimizer=opt)
+
+        if "monitor" in self.hparams:
+            scheduler = {'scheduler': scheduler, 'monitor': self.hparams.monitor.metric_to_track}
+
+        return [opt], [scheduler]
