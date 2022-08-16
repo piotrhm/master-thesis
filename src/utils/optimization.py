873058d24d99f9f4b00f376fa721ced4dc8ac45d
piotrhm <helm.piotr@gmail.com>
2022-08-16 10:27:24 +0200

    new lr scheduler

---
+++ src/utils/optimization.py
@@ -0,0 +1,13 @@
+from torch.optim.lr_scheduler import ConstantLR, ExponentialLR, SequentialLR
+
+
+def get_constant_with_exponential_lr(
+    optimizer,
+    factor=0.1,
+    total_iters_first=5,
+    gamma=0.9
+):
+    scheduler1 = ConstantLR(optimizer, factor=factor, total_iters=total_iters_first)
+    scheduler2 = ExponentialLR(optimizer, gamma=gamma)
+
+    return SequentialLR(optimizer, schedulers=[scheduler1, scheduler2], milestones=[2])
