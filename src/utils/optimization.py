0791a5b542f5421f495469c1bd286b30f7f55e7f
piotrhm <helm.piotr@gmail.com>
2022-08-16 12:05:10 +0200

    add new arg to lr scheduler fix

--- src/utils/optimization.py
+++ src/utils/optimization.py
@@ -6,8 +6,10 @@ def get_constant_with_exponential_lr(
     factor=0.1,
     total_iters_first=5,
     gamma=0.9,
-    milestones=[2]
+    milestones=None
 ):
+    if milestones is None:
+        milestones = [2]
     scheduler1 = ConstantLR(optimizer, factor=factor, total_iters=total_iters_first)
     scheduler2 = ExponentialLR(optimizer, gamma=gamma)
 
